cbca4b7aaac8f95e12963dcc0d51ff18784325c8
furqonstudio <furqonstudio@gmail.com>
2023-06-26 21:35:42 +0700

    Fix : remove unused import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
-import { logo } from '../utils/constants'
 import SearchBar from './SearchBar'
 
 const Navbar = () => (
